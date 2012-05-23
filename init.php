2cd3a823e5095a2b907b71e1241344e644bf6fa1
hamaco <hamanaka.kazuhiro@gmail.com>
2012-05-23 12:22:50 +0900

    これはひどい

---
+++ init.php
@@ -0,0 +1,33 @@
+<?php
+
+$pluginContainer = ClassRegistry::getObject('PluginContainer');
+$pluginContainer->installed('cc_u-nya-', '0.1');
+
+App::uses('CakeEventManager', 'Event');
+CakeEventManager::instance()->attach(function ($event)
+  {
+    $i = 0;
+    $event->result['text'] = preg_replace_callback("/u-nya-/", function ($matches) use (&$i) {
+      $i++;
+      return <<<EOM
+<span id="cc-u-nya-$i"></span><script>
+  (function() {
+    var aa = [
+      "（」・ω・）」うー！",
+      "（／・ω・）／にゃー！",
+      "（」・ω・）」うー！",
+      "（／・ω・）／にゃー！",
+      "（」・ω・）」うー！",
+      "（／・ω・）／にゃー！",
+      "Let's＼(・ω・)／にゃー！",
+      "Let's＼(・ω・)／にゃー！"
+    ], span = document.getElementById("cc-u-nya-$i"), i = 0;
+    setInterval(function() {
+      span.innerHTML = aa[i++];
+      i = i % aa.length;
+    }, 500);
+  })();
+</script>
+EOM;
+    }, $event->data['text']);
+  }, 'Helper.Candy.afterTextilizable');
